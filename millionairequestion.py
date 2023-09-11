7da641bcebee7e3793a51d250ede77cd2279068a
jmgarci9 <51499306+jmgarci9@users.noreply.github.com>
2023-09-11 02:54:39 -0700

    Question Class test complete

--- millionairequestion.py
+++ millionairequestion.py
@@ -1,2 +1,15 @@
+import random
+
 class Questions:
-    pass
+    def ___init___(self, id: int):
+        self.id = id
+        self.question = ""
+
+    def random_question(self):
+        
+        if(self.id == 1)
+            question = "What animal can be seen on the Porsche logo?"
+        
+    
+    return self.question
+
