539daa054516062b18bd17d16fb1ad194ce5f4ef
jmgarci9 <51499306+jmgarci9@users.noreply.github.com>
2023-09-11 04:01:36 -0700

    Questions Class Fixed

--- millionairequestion.py
+++ millionairequestion.py
@@ -1,23 +1,27 @@
 import random
 
 class Questions:
-    def ___init___(self, id_question: int):
+    def __init__(self, id_question:int):
         self.id_question = id_question
         self.question = ""
 
-    def random_question(self):
+    def random_question(self, id_answer: int):
         
-        if(self.id == 1)
-            question = "What animal can be seen on the Porsche logo?"
+        if(id_answer == 1):
+            self.question = "What animal can be seen on the Porsche logo?"
         
+        return self.question
     
-    return self.question
 
-class Answers:
-    def ___init___(self, id_answer: int, choice: int):
+
+"""class Answers:
+    def __init__(self, id_answer: int, choice: int):
         self.id_answer = id_answer
         self.choice= choice
 
-    def answer(self):
+    def answer(self):"""
 
-        
+example=Questions(6)
+print(example.random_question(1))
+
+
